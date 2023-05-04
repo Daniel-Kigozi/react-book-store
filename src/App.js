0686d4b23f7081150443e4789726236740342b26
Daniel-Kigozi <kdanalex69@gmail.com>
2023-05-04 17:03:39 +0300

    add components for books and categories

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Categories from './components/categories';
+import Books from './components/books';
+import { Navbar } from './components/nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+
+      </Routes>
+
+    </BrowserRouter>
+
   );
 }
 
