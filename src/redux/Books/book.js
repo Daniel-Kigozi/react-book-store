d64c3e0e3cd2614463683fd857998226fa82e071
Daniel-Kigozi <kdanalex69@gmail.com>
2023-05-11 14:02:55 +0300

    add api to components

--- src/redux/Books/book.js
+++ src/redux/Books/book.js
@@ -1,41 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const bookSlice = createSlice({
-  name: 'books',
-  initialState: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+const ADD_BOOK = 'react-bookstore/books/ADD_BOOK';
+const DELETE_BOOK = 'react-bookstore/books/DELETE_BOOK';
+const BOOK_FAILURE = 'react-bookstore/books/BOOK_FAILURE';
 
-  reducers: {
-    addBook: (state, action) => {
-      const newBook = {
-        id: action.payload.item_id,
-        title: action.payload.title,
-        author: action.payload.author,
-      };
-      state.push(newBook);
-    },
-    removeBook: (state, action) => state.filter((book) => book.key !== action.payload.id),
-  },
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps';
+const identifier = 'xZalG8etSb0nJTXORfm0';
 
-});
+const books = [];
 
-export const { addBook, removeBook } = bookSlice.actions;
-export default bookSlice.reducer;
+const BookReducer = (state = books, action) => {
+  switch (action.type) {
+    case ADD_BOOK: {
+      const books = Object.entries(action.payload);
+      return books.map((book) => ({
+        id: book[0],
+        ...book[1][0],
+      }));
+    }
+    case DELETE_BOOK:
+      return [...state.filter((book) => (book.id !== action.payload))];
+    default:
+      return state;
+  }
+};
+
+export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
+
+export const getBooks = () => async (dispatch) => {
+  await axios.get(`${url}/${identifier}/books`).then(
+    (response) => dispatch(addBook(response.data)),
+    (err) => dispatch({ type: BOOK_FAILURE, err }),
+  );
+};
+
+export const deleteBook = (id) => async (dispatch) => {
+  await axios
+    .delete(`${url}/${identifier}/books/${id}`)
+    .then(() => dispatch(getBooks())); return { type: DELETE_BOOK, payload: id };
+};
+
+export const postBook = (book) => async (dispatch) => {
+  await axios
+    .post(`${url}/${identifier}/books`, book)
+    .then(() => dispatch(getBooks()));
+};
+
+export default BookReducer;
